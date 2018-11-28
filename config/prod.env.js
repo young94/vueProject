0bb0d30afbbe224b3f9256042f254f669fca0685
yangjing <young_Jing@163.com>
2018-11-28 16:34:43 +0800

    ，，

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   BASE_API: '"http://192.168.2.8/api"',
   BASE_URL: '"http://192.168.2.8"',
-  // BASE_API: '"http://www.yuanqiutianxia.com/api"',
-  // BASE_URL: '"http://www.yuanqiutianxia.com"',//网站地址
+  // BASE_API: '"http://www.tuliaoshijie.com/api"',
+  // BASE_URL: '"http://www.tuliaoshijie.com"',//网站地址
 }
