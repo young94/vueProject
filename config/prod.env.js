beeeba21936ef6ac70fbc9fb69e5530cfad07ba7
yangjing <young_Jing@163.com>
2018-11-28 15:17:01 +0800

    测试

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,8 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  BASE_API: '"http://192.168.2.8/api"',
+  BASE_URL: '"http://192.168.2.8"',
+  // BASE_API: '"http://www.yuanqiutianxia.com/api"',
+  // BASE_URL: '"http://www.yuanqiutianxia.com"',//网站地址
 }
